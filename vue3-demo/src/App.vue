fac514f83988967e95db596423442e6778a4564b
ODD <oncwnuJ9mMi_bm0xx0eTWyWQTuk8@git.weixin.qq.com>
2020-09-30 16:23:20 +0800

    vue-demo

---
+++ vue3-demo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="addMyCount">count is: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+import { ref, isRef } from 'vue'
+export default {
+  name: 'App',
+  // data() {
+  //   return {
+  //     count: 10086
+  //   }
+  // },
+  // methods: {
+  //   add() {
+  //     this.count += 1000
+  //   }
+  // }
+  setup() {
+    // debugger
+    const count = ref(10086)
+    function addMyCount() {
+      // debugger
+      count.value += 100
+    }
+    return {
+      count,
+      addMyCount
+    }
+  }
+}
+</script>
---
+++ vue3-demo/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="add">count is: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+import { ref, isRef } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  // data() {
+  //   return {
+  //     count: 0
+  //   }
+  // }
+  setup(props) {
+    debugger
+    const count = ref(10086)
+    function add() {
+      count.value += 100
+    }
+    return {
+      count,
+      add
+    }
+  }
+}
+</script>
